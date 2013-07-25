184aef6ec4350f88d15ac249c4321e166e51cb14
Alec McEachran <alec@alecmce.com>
2013-07-25 15:56:09 -0700

    Updates tests

--- test/dust/tween/systems/TweenSystemTest.hx
+++ test/dust/tween/systems/TweenSystemTest.hx
@@ -4,13 +4,13 @@ import dust.tween.data.Tween;
 import dust.context.Context;
 import dust.entities.Entities;
 import dust.entities.Entity;
-import dust.systems.impl.Systems;
 import dust.systems.impl.SystemsList;
-import dust.systems.impl.SystemsLoop;
 import dust.tween.TweenConfig;
 
 import dust.Injector;
 import flash.display.Sprite;
+
+using dust.tween.TweensUtil;
 
 class TweenSystemTest
 {
@@ -32,7 +32,7 @@ class TweenSystemTest
     @Test public function pulseSystemModifiesEntityPulses()
     {
         var tween = new Tween(0, 1, 10);
-        entity.add(tween);
+        entity.addTween(tween);
         systems.update(1);
         Assert.areEqual(0.1, tween.value);
     }
